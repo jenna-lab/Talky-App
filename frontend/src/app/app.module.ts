f968c2e456c7108aa24772e848e1cfd1160be450
jenna <jensam209@gmail.com>
2023-12-07 14:49:29 +0300

    myprofile component,add like and comment

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyprofileComponent } from './myprofile/myprofile.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     RegisterComponent,
     LoginComponent,
     ForgotPasswordComponent,
-    EditProfileComponent
+    EditProfileComponent,
+    MyprofileComponent
   ],
   imports: [
     BrowserModule,
